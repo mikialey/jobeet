472b82881516c63d948ceb9cacd4bdf2a5d72218
Baatezus <>
2017-03-10 17:47:29 +0100

    Squelette de base de l'application

---
+++ src/JobeetBundle/Entity/Category.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace JobeetBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Category
+ *
+ * @ORM\Table(name="category")
+ * @ORM\Entity(repositoryClass="JobeetBundle\Repository\CategoryRepository")
+ */
+class Category
+{
+    
+    /**
+     * Many Groups have Many Users.
+     * @ORM\ManyToMany(targetEntity="Affiliate", mappedBy="category")
+     */
+    private $affiliates;
+    
+    /**
+     * @ORM\OneToMany(targetEntity="Job", mappedBy="category")
+     */
+    private $jobs;
+
+    public function __construct()
+    {
+        $this->jobs = new ArrayCollection();
+        $this->affiliates = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+    
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Category
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
